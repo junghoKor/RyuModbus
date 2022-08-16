c582934a2ad78e04f58c2f906f79be80be56ffc2
Jungho Ryu <46273970+junghoKor@users.noreply.github.com>
2022-08-16 19:29:11 +0900

    Add files via upload
    first release

---
+++ Test1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+using RyuSerial;
+
+namespace Test1
+{
+    class Program
+    {
+
+        static Int64 g_val = 0xFFFFFFFF;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World.");
+
+
+            //var thread1 => { int i = 0; };
+
+            Func<int> f1 = () => { int i = 0; return 0; };
+
+            Thread t1 = new Thread(t1func);
+            Thread t2 = new Thread(t2func);
+
+            t1.Start();
+            t2.Start();
+
+
+            RyuModbus  s1 = new RyuModbus();
+
+
+            s1.InitThread();
+
+
+            Thread.Sleep(10000);
+
+
+            s1.EndThread();
+
+
+
+            t1.Join();
+            t2.Join();
+
+            int k = 0;
+        }
+
+
+        static void t1func()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                g_val++;
+                g_val--;
+            }
+        }
+
+
+        static void t2func()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                Int64 val = g_val;
+                if ( false == (val == 0xFFFFFFFF || val == 0x100000000))
+                {
+                    Console.WriteLine("g_val err = {0:x8}", val);
+                }
+            }
+        }
+    }
+}
---
+++ Test1/RyuModbus/SerialPort.cs
@@ -0,0 +1,89 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+using System.IO;
+using System.IO.Ports;
+using System.Diagnostics;   // Debug.WriteLine()
+
+namespace RyuSerial
+{
+    class SimpleSerial
+    {
+        private SerialPort _serialPort = new SerialPort();
+
+        public bool OpenPort( string portName, int baudRate, Parity parity = Parity.None, int databits = 8, StopBits stopbits = StopBits.One, Handshake handshake = Handshake.None )
+        {
+            _serialPort.PortName = portName;        // COM1
+            _serialPort.BaudRate = baudRate;        // 9600
+            _serialPort.Parity = parity;            // None
+            _serialPort.DataBits = databits;        // 8
+            _serialPort.StopBits = stopbits;        // 1
+            _serialPort.Handshake = handshake;      // default is None
+            // default timeout set
+            _serialPort.ReadTimeout = 100;
+            _serialPort.WriteTimeout = 100;
+
+            try
+            {
+                _serialPort.Open();
+            }
+            catch(IOException e)
+            {
+                Debug.WriteLine("SerialPort {0} open error : {1}", portName, e.Message);
+                return false;
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine("SerialPort {0} open error : argument err", portName);
+                return false;
+            }
+            return true;
+        }
+
+        public bool SetCommunicationTimeout( int readTimeout, int writeTimeout )
+        {
+            _serialPort.ReadTimeout = readTimeout;
+            _serialPort.WriteTimeout = writeTimeout;
+            return true;
+        }
+
+
+        public bool ReadBytes( Byte [] buff, int size )
+        {
+            try
+            {
+                int readSize = _serialPort.Read(buff, 0, size);
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine("_serialPort.Read() exception : {0}", e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool WriteBytes( Byte [] buff, int size )
+        {
+            try
+            {
+                _serialPort.Write(buff, 0, size);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("_serialPort.Write() exception : {0}", e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        public void ClosePort()
+        {
+            _serialPort.Close();
+        }
+    }
+}
